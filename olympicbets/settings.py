cf70e45bb6c49387f1d33345f780daf85357ff56
Willian Frantz <willian.frantz@deway.com.br>
2017-04-03 11:25:12 -0300

    fixes

--- olympicbets/settings.py
+++ olympicbets/settings.py
@@ -1,28 +1,3 @@
-'''# Django project settings loader
-import os
-ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
-
-# You can key the configurations off of anything - I use project path.
-configs = {
-    '/Users/wlsf/Desktop/wlsf_projects/studies/django/olympicbets/olympicbets': 'settings_development',
-    'server': 'settings_production'
-}
-
-# Import the configuration settings file - REPLACE projectname with your project
-
-if configs[ROOT_PATH]:
-    cfg = configs[ROOT_PATH]
-else:
-    cfg = configs['server']
-
-config_module = __import__('olympicbets.%s' % cfg, globals(), locals(), 'olympicbets')
-
-# Load the config settings properties into the local scope.
-for setting in dir(config_module):
-    if setting == setting.upper():
-        locals()[setting] = getattr(config_module, setting)'''
-
-
 """
 Django settings for olympicbets project.
 
@@ -101,12 +76,8 @@ WSGI_APPLICATION = 'olympicbets.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'olympicbets',
-        'USER': 'olympicbetsadmin',
-        'PASSWORD': 'd41d8cd98f00b204e98',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
