9ad827cd0cdbf3e67b5be6481cd08b23c19dbe3d
Willian Frantz <willian.frantz@deway.com.br>
2017-04-12 13:59:04 -0300

    Fix status missing on import

--- api/v1/leagues/views.py
+++ api/v1/leagues/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from rest_framework.response import Response
 
 from api.v1.leagues.models import League
